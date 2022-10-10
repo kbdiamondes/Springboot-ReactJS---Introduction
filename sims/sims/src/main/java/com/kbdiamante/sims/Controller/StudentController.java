780a98af6c03503a98e16cdd87cd6e9c704a908f
Keith <68324799+kbdiamondes@users.noreply.github.com>
2022-10-10 11:22:00 +0800

    Update and Delete Functionality

--- sims/sims/src/main/java/com/kbdiamante/sims/Controller/StudentController.java
+++ sims/sims/src/main/java/com/kbdiamante/sims/Controller/StudentController.java
@@ -4,8 +4,11 @@ package com.kbdiamante.sims.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,5 +48,17 @@ public class StudentController{
 	@GetMapping("/getByFirstname")
 	public StudentEntity findByFirstname(@RequestParam String firstname) {
 		return stserv.findByFirstname(firstname);
+	}
+	
+	//Update a record
+	@PutMapping("/putStudent")
+	public StudentEntity putStudent(@RequestParam int id, @RequestBody StudentEntity newStudentDetails) throws Exception{
+		return stserv.putStudent(id, newStudentDetails);
+	}
+	
+	//Delete a record
+	@DeleteMapping("/deleteStudent/{id}")
+	public String deleteStudent(@PathVariable int id) {
+		return stserv.deleteStudent(id);
 	}
 }
--- sims/sims/src/main/java/com/kbdiamante/sims/Service/StudentService.java
+++ sims/sims/src/main/java/com/kbdiamante/sims/Service/StudentService.java
@@ -1,6 +1,7 @@
 package com.kbdiamante.sims.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -37,5 +38,41 @@ public class StudentService {
 			return srepo.findByFirstname(firstname);
 		else
 			return null;
+	}
+	
+	//U - Update a student record
+	
+	public StudentEntity putStudent(int id, StudentEntity newStudentDetails) throws Exception{
+		
+		StudentEntity student = new StudentEntity();
+
+		try {
+			//steps in updating
+			//Step 1
+			student = srepo.findById(id).get(); //findById() is a pre-defined method
+			
+			//Step 2 - update the record
+			student.setCourse(newStudentDetails.getCourse());
+			student.setYearlevel(newStudentDetails.getYearlevel());
+			
+			//Step 3 -  save the information and return the value
+			return srepo.save(student); 
+			
+		}catch(NoSuchElementException nex) {
+			throw new Exception("ID Number " + id + " does not exist!"); 
+		}
+	}
+	
+	//D - Delete student record
+	public String deleteStudent(int id) {
+		String msg;
+		if(srepo.findById(id) !=null) {
+			srepo.deleteById(id);
+			
+			msg = "Student ID Number " + id + " is successfully deleted!";
+		}else
+			msg = "Student ID Number " + id + " is NOT found!";
+		
+		return msg;
 	}
 }
