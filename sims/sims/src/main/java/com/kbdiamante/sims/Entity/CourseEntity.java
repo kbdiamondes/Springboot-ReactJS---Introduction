de3e9daf2590e0a8f4117ae76c7eaa9d5d3884ba
Keith <kbdiamantes@gmail.com>
2022-10-17 11:18:07 +0800

    One to Many field on Student

--- sims/sims/src/main/java/com/kbdiamante/sims/Entity/CourseEntity.java
+++ sims/sims/src/main/java/com/kbdiamante/sims/Entity/CourseEntity.java
@@ -1,7 +1,13 @@
 package com.kbdiamante.sims.Entity;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 
@@ -10,55 +16,66 @@ import javax.persistence.Table;
 public class CourseEntity {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int courseid; 
 	private String code; 
 	private String descr; 
 	private int unit;
 	
-	public CourseEntity() {}
+	@OneToMany(cascade = CascadeType.MERGE)
+	private Set<StudentEntity> student; 
 	
-	public CourseEntity(int courseid, String code, String descr, int unit) {
+	public CourseEntity() {}
+
+	public CourseEntity(int courseid, String code, String descr, int unit, Set<StudentEntity> student) {
 		super();
 		this.courseid = courseid;
 		this.code = code;
 		this.descr = descr;
 		this.unit = unit;
+		this.student = student;
 	}
-
 
 	public int getCourseid() {
 		return courseid;
 	}
-	
-	
+
+	public void setCourseid(int courseid) {
+		this.courseid = courseid;
+	}
+
 	public String getCode() {
 		return code;
 	}
-	
-	
+
 	public void setCode(String code) {
 		this.code = code;
 	}
-	
-	
+
 	public String getDescr() {
 		return descr;
 	}
-	
-	
+
 	public void setDescr(String descr) {
 		this.descr = descr;
 	}
-	
-	
+
 	public int getUnit() {
 		return unit;
 	}
-	
-	
+
 	public void setUnit(int unit) {
 		this.unit = unit;
-	} 
+	}
+
+	public Set<StudentEntity> getStudent() {
+		return student;
+	}
+
+	public void setStudent(Set<StudentEntity> student) {
+		this.student = student;
+	}
+	
 	
 	
 	
